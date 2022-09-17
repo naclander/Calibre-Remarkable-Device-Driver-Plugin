a156f4554d07e08867018fb03ea7f29152ae2e4e
Kenivia <kenivia.fan@gmail.com>
2022-09-17 13:02:56 +1200

    fixed installing with generated zip file, 3.10 check

--- create_plugin_zip.sh
+++ create_plugin_zip.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+pyv="$(python -V 2>&1)"
+if ! [[ $pyv =~ ^"Python 3.10."* ]];
+then
+	echo Python version must be 3.10
+	exit 1
+fi
+	
 set -euxo pipefail
 # Helper script to generate the plugin zip file which can be directly
 # installed by Calibre
